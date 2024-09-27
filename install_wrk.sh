b5ff7a30cc237476ad3309d3b6b4d71e52ea7b30
DuncanZhang <duncan751122@gmail.com>
2024-09-27 11:59:43 +0800

    Update install_wrk.sh

--- install_wrk.sh
+++ install_wrk.sh
@@ -8,13 +8,16 @@ sudo apt update && sudo apt upgrade -y
 echo -e "\033[34m[TASK 2] Install dependencies tools\033[0m"
 sudo apt install build-essential libssl-dev git unzip
 
-echo -e "\033[34m[TASK 3] Install wrk tool\033[0m"
-sudo cd wrk
+echo -e "\033[34m[TASK 3] Git wrk repo\033[0m"
+git clone https://github.com/wg/wrk.git wrk
+
+echo -e "\033[34m[TASK 4] Install wrk tool\033[0m"
+cd wrk
 sudo make
 sudo cp wrk /usr/local/bin
 
-echo -e "\033[34m[TASK 3] Remove script and file \033[0m"
-sudo cd ..
+echo -e "\033[34m[TASK 5] Remove script and file \033[0m"
+cd ..
 sudo rm -rf wrk install_wrk.sh 
 
 echo -e "\033[34m===============Automatic installation completed===============\033[0m"
